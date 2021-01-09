adeb25cc842fc566f2dfe470d12c71995d7d778e
ateamproject2021 <77202560+ateamproject2021@users.noreply.github.com>
2021-01-09 13:38:37 -0500

    Create Workingfile.py

---
+++ Workingfile.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+#import banner head with "tagline here"
+#open up a log file
+#read it line by line
+#search for specific parameters within the file
+#if parameters are met
+#output dersired string
+#if parameters are not found
+#error message
+
