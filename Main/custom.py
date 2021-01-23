60145b310106e695c672bbc0a01f8245ad99e401
Stephen <68256430+skoplik1@users.noreply.github.com>
2021-01-23 11:02:51 -0500

    Create custom.py

---
+++ Main/custom.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sys
+
+def main():
+    #collector result string
+    result = ""
+
+    #this code allows for file's contents to be used as input in our function
+    with open(sys.argv[1], 'r') as f:
+        contents = f.readlines()
+    #the "meat" of the test case module
+    print("Enter your custom search parameter:")
+    user_input = input()
+
+    for line in contents:
+    #insert specific test case criteria here
+        if user_input in line:
+                result += line
+
+    if len(result) < 5:
+        print("No applicable log entries found.")
+    else: print(result)
+
+#no touchy
+if __name__ == "__main__":
+    main()
